f99c675c1564618e7fe9eefe429b0466b5a29b1c
cuki <mauriciocukier@gmail.com>
2015-09-15 00:50:12 -0300

    Read Write transactions

--- src/RF1279.h
+++ src/RF1279.h
@@ -8,11 +8,16 @@
 #ifndef RF1279_H_
 #define RF1279_H_
 
-#define data_size 12
+#define data_size 		12
+#define command_size	23
 
 typedef enum command_yy_enum {
 	cmd_write = 1, cmd_read, cmd_standard, cmd_central, cmd_node
 } command_yy_t;
+
+typedef enum command_xx_enum {
+	cmd_xx_response = 0x00, cmd_xx_sending = 0x80
+} command_xx_t;
 
 typedef enum baud_rate_enum {
 	b1200bps = 1, b2400bps, b4800bps, b9600bps, b19200bps, b38400bps, b57600bps
@@ -27,8 +32,8 @@ typedef enum rf_factor_enum {
 } rf_factor_t;
 
 typedef enum mode_enum {
-	standard, low_power, sleep
-} mode_t;
+	mode_standard, mode_low_power, mode_sleep
+} radio_mode_t;
 
 typedef enum rf_bw_enum {
 	bw_625k = 6, bw_125k, bw_256k, bw_512k
@@ -38,39 +43,38 @@ typedef enum rf_power_enum {
 	p_4dBm = 1, p_7dBm, p_10dBm, p_13dBm, p_14dBm, p_17dBm, p_20dBm
 } rf_power_t;
 
-typedef struct data_str {
+typedef struct radio_data_str {
 	baud_rate_t baudrate;
 	parity_t parity;
-	unsigned char float_hl;
-	unsigned char float_lh;
-	unsigned char float_ll;
+	float frequencie;
 	rf_factor_t rf_factor;
-	mode_t mode;
+	radio_mode_t mode;
 	rf_bw_t rf_bw;
-	unsigned char id_hi;
-	unsigned char id_lo;
+	unsigned short id;
 	unsigned char net_id;
 	rf_power_t rf_power;
-} data_str_t;
+} radio_data_t;
 
-typedef struct read_command_str {
-	unsigned short sync_word;
-	unsigned short id_code;
-	unsigned char header;
-	unsigned char command_xx;
-	unsigned char command_yy;
-	unsigned char length;
-	unsigned char data[data_size];
-	unsigned char cs;
-	unsigned short end_code;
-} read_command_str_t;
-
-typedef union read_command_un {
-	read_command_str_t str;
-	unsigned char raw[sizeof(read_command_str_t) - 1];
-} read_t;
-
-unsigned char crc(unsigned char *data, unsigned char size);
-int make_radio_read_request(unsigned char *request);
+extern unsigned char crc(unsigned char *data, unsigned char size);
+extern unsigned char * make_radio_request(command_yy_t cmd_type,
+		unsigned char *data, int size);
+extern unsigned char * make_radio_read_command(int size);
+extern int make_radio_read_transaction(int fd, unsigned char * response);
+extern float freqtof(unsigned char freq_hl, unsigned char freq_lh,
+		unsigned char freq_ll);
+extern radio_data_t get_radio_data(int fd);
+extern unsigned char touchar(int in, int index);
+extern unsigned char * ftouchar(float frequencie);
+extern unsigned char * make_radio_write_command(radio_data_t data);
+extern int make_radio_write_transaction(int fd, radio_data_t data);
+extern int write_radio_baudrate(int fd, baud_rate_t baudrate);
+extern int write_radio_parity(int fd, parity_t parity);
+extern int write_radio_frequencie(int fd, float frequencie);
+extern int write_radio_rf_factor(int fd, rf_factor_t rf_factor);
+extern int write_radio_mode(int fd, radio_mode_t mode);
+extern int write_radio_rf_bw(int fd, rf_bw_t rf_bw);
+extern int write_radio_id(int fd, unsigned short id);
+extern int write_radio_net_id(int fd, unsigned char net_id);
+extern int write_radio_rf_power(int fd, rf_power_t power);
 
 #endif /* RF1279_H_ */
